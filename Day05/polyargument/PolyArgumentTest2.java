32c3c7b4237a9be81d26c146d091826905a914d4
Cellularhacker <wva3cdae@gmail.com>
2017-09-13 10:43:41 +0900

    Fixed the example Array into Vector.

--- Day05/polyargument/PolyArgumentTest2.java
+++ Day05/polyargument/PolyArgumentTest2.java
@@ -1,6 +1,8 @@
 //2017-09-13 KST PolyArgumentTest2.java
 package polyargument;
 
+import java.util.*;
+
 class Product {
 	int price;
 	int bonusPoint;
@@ -49,8 +51,7 @@ class Audio extends Product {
 class Buyer {
 	int money = 1000;
 	int bonusPoint = 0;
-	Product[] item = new Product[10];
-	int i = 0;
+	Vector item = new Vector();
 	
 	void buy(Product p) {
 		if(money < p.price) {
@@ -60,18 +61,33 @@ class Buyer {
 		
 		money -= p.price;
 		bonusPoint += p.bonusPoint;
-		item[i++] = p;
+		item.add(p);
 		System.out.println(p + "을/를 구입하셨습니다.");
+	}
+	
+	void refund(Product p) {
+		if(item.remove(p)) {
+			money += p.price;
+			bonusPoint -= p.bonusPoint;
+			System.out.println(p + "을/를 반품하셨습니다.");
+		} else {
+			System.out.println("구입하신 제품 중 해당 제품이 없습니다.");
+		}
 	}
 	
 	void summary() {
 		int sum = 0;
 		String itemList = "";
 		
-		for(int i=0; i<item.length; i++) {
-			if(item[i] == null) break;
-			sum += item[i].price;
-			itemList += item[i] + ", ";
+		if(item.isEmpty()) {
+			System.out.println("구입하신 제품이 없습니다.");
+			return;
+		}
+		
+		for(int i=0; i<item.size(); i++) {
+			Product p = (Product)item.get(i);
+			sum += p.price;
+			itemList += (i==0) ? "" + p : ", " + p;
 		}
 		
 		System.out.print("구입하신 물품의 총금액은 " + sum + "만원입니다.");
@@ -88,6 +104,9 @@ public class PolyArgumentTest2 {
 		b.buy(tv);
 		b.buy(com);
 		b.buy(audio);
+		b.summary();
+		
+		b.refund(com);
 		b.summary();
 
 	}
